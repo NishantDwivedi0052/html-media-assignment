f501e03e3fa65653f42a132f7c925faa57c8e640
NishantDwivedi0052 <159905788+NishantDwivedi0052@users.noreply.github.com>
2024-02-13 18:40:00 +0530

    Add files via upload

---
+++ nextline.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"Physics"<<endl;
+    // cout<<"Wallah";
+
+   // cout<<"Physics"<<endl<<"Wallah";
+
+   cout<<"Physics\nWallah";
+}
+    
---
+++ sizeofop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int x;
+    //float x;
+    //bool x;
+    //char x;
+   // short int x;
+   long long x;
+    cout<<sizeof(x);
+}
